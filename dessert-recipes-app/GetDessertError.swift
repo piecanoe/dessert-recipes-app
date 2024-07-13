676ccba9b5a078f374c571dd753c59b01e5cc26c
piecanoe <karenkaydu2@gmail.com>
2024-07-13 15:08:56 -0700

    Refactor GetDessertError into separate file

---
+++ dessert-recipes-app/GetDessertError.swift
@@ -0,0 +1,14 @@
+//
+//  GetDessertError.swift
+//  dessert-recipes-app
+//
+//  Created by Karen Du on 7/13/24.
+//
+
+import Foundation
+
+enum GetDessertError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
