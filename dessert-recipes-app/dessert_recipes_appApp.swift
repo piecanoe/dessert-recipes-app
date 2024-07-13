56c74b7ff69acc5e8a9f8ca07e8bf5a8cbe3ae24
piecanoe <karenkaydu2@gmail.com>
2024-07-13 14:25:31 -0700

    Initial Commit

---
+++ dessert-recipes-app/dessert_recipes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dessert_recipes_appApp.swift
+//  dessert-recipes-app
+//
+//  Created by Karen Du on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct dessert_recipes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
