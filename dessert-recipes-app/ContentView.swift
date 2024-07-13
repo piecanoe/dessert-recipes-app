a5473d0479c062eddcdec80a7aa393d295d4b672
piecanoe <karenkaydu2@gmail.com>
2024-07-13 14:45:12 -0700

    Create getDessert function to fetch data from Free Meal API, create GetDessertError enum to handle errors, move structs and enum into ContentView struct

--- dessert-recipes-app/ContentView.swift
+++ dessert-recipes-app/ContentView.swift
@@ -17,20 +17,52 @@ struct ContentView: View {
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func getDessert() async throws -> DessertItem {
+        let endpoint = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+        
+        guard let url = URL(string: endpoint) else {
+            throw GetDessertError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GetDessertError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let wrapper = try decoder.decode(Wrapper.self, from: data)
+            guard let firstDessert = wrapper.meals.first else {
+                throw GetDessertError.invalidData
+            }
+            return firstDessert
+        } catch {
+            throw GetDessertError.invalidData
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct Wrapper: Codable {
+        let meals: [DessertItem]
+    }
+    
+    struct DessertItem: Codable {
+        let strMeal: String
+        let strMealThumb: String
+        let idMeal: String
+    }
+    
+    enum GetDessertError: Error {
+        case invalidURL
+        case invalidResponse
+        case invalidData
     }
 }
 
-struct Wrapper: Codable {
-    let meals: [DessertItem]
-}
-
-struct DessertItem: Codable {
-    let strMeal: String
-    let strMealThumb: String
-    let idMeal: String
-}
