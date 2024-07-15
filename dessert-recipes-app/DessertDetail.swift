23567e6b94f1426e5e4258f6d3b8e1ba5e9e2803
piecanoe <karenkaydu2@gmail.com>
2024-07-14 21:36:59 -0700

    Create DessertDetail View

---
+++ dessert-recipes-app/DessertDetail.swift
@@ -0,0 +1,20 @@
+//
+//  DessertDetail.swift
+//  dessert-recipes-app
+//
+//  Created by Karen Du on 7/14/24.
+//
+
+import SwiftUI
+
+struct DessertDetail: View {
+    var body: some View {
+        Text("Dessert Detail")
+    }
+}
+
+struct DessertDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        DessertDetail()
+    }
+}
