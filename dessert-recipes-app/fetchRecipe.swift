9a22d2cf92551ec7978cebeb6cda8171c840638b
piecanoe <karenkaydu2@gmail.com>
2024-07-14 21:36:23 -0700

    Refactor fetchDessert and fetchRecipe

--- dessert-recipes-app/fetchRecipe.swift
+++ dessert-recipes-app/fetchRecipe.swift
@@ -1,5 +1,5 @@
 //
-//  fetchDessert.swift
+//  fetchRecipe.swift
 //  dessert-recipes-app
 //
 //  Created by Karen Du on 7/14/24.
@@ -7,27 +7,29 @@
 
 import Foundation
 
-enum GetDessertError: Error {
+enum GetRecipeError: Error {
     case invalidURL
     case invalidResponse
     case invalidData
 }
 
-func fetchDessert() async throws -> [DessertItem] {
-    let endpoint = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+func fetchRecipe(for idMeal:String) async throws -> [Recipe] {
+    let endpoint = "https://themealdb.com/api/json/v1/1/lookup.php?i\(idMeal)"
     
     guard let url = URL(string: endpoint) else {
-        throw GetDessertError.invalidURL
+        throw GetRecipeError.invalidURL
     }
     
-    let (data, response) = try await URLSession.shared.data(from: url)
+    let (data, response ) = try await
+    URLSession.shared.data(from: url)
     
     guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
         throw GetDessertError.invalidResponse
     }
     
     let decoder = JSONDecoder()
-    let wrapper = try decoder.decode(Wrapper.self, from: data)
+    let wrapper = try decoder.decode(RecipeWrapper.self, from: data)
     
     return wrapper.meals
+    
 }
