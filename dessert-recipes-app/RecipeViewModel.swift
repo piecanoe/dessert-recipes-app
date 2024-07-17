b491ed3a9ba42cd76e16c41aec7c5c146067cc89
piecanoe <karenkaydu2@gmail.com>
2024-07-17 13:23:54 -0700

    Create computed property for "ingredients" and "measures" that include array of all ingredients and all measures

--- dessert-recipes-app/RecipeViewModel.swift
+++ dessert-recipes-app/RecipeViewModel.swift
@@ -12,6 +12,8 @@ struct RecipeWrapper: Codable {
 }
 
 struct Recipe: Codable, Identifiable, Hashable {
+    var id: String { idMeal }
+    
     let idMeal: String
     let strMeal: String
     let strInstructions: String
@@ -55,6 +57,56 @@ struct Recipe: Codable, Identifiable, Hashable {
     let strMeasure18: String?
     let strMeasure19: String?
     let strMeasure20: String?
+    
+    var ingredients : [String] {
+        return [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+            strIngredient6,
+            strIngredient7,
+            strIngredient8,
+            strIngredient9,
+            strIngredient10,
+            strIngredient11,
+            strIngredient12,
+            strIngredient13,
+            strIngredient14,
+            strIngredient15,
+            strIngredient16,
+            strIngredient17,
+            strIngredient18,
+            strIngredient19,
+            strIngredient20,
+        ].compactMap { $0 }.filter { !$0.isEmpty }
+    }
+    
+    var measures : [String] {
+        return [
+            strMeasure1,
+            strMeasure2,
+            strMeasure3,
+            strMeasure4,
+            strMeasure5,
+            strMeasure6,
+            strMeasure7,
+            strMeasure8,
+            strMeasure9,
+            strMeasure10,
+            strMeasure11,
+            strMeasure12,
+            strMeasure13,
+            strMeasure14,
+            strMeasure15,
+            strMeasure16,
+            strMeasure17,
+            strMeasure18,
+            strMeasure19,
+            strMeasure20,
+        ].compactMap { $0 }.filter { !$0.isEmpty }
+    }
 
-    var id: String { idMeal }
 }
+
